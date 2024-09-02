6ca9e2b41db2967e6238c7e848c5d608fe771bf5
brayan979 <brayan111ga@gmail.com>
2024-09-02 14:10:35 +0200

    delete blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brayan979.github.io',
-  base: 'mi-repo',
 
   integrations: [mdx(), sitemap(), tailwind()]
 });
